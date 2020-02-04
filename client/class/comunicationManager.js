be36252a139a6bbae5d81113f244d44fca1c8f35
Simone <benigni_simone@libero.it>
2020-02-04 19:52:47 +0100

    fix vari

--- client/class/comunicationManager.js
+++ client/class/comunicationManager.js
@@ -2,44 +2,10 @@ class ComunicationManager
 {
     constructor()
     {
-        this.UrlMain = "http://127.0.0.1:8200/"
+
 
     }
 
 
-    httpGet(url, callback)
-    {
-        var request = new XMLHttpRequest();
-        request.onreadystatechange=function()
-        {
-            if (request.readyState==4 && request.status==200)
-            {
-                if(callback != undefined)
-                {
-                    callback(JSON.parse(request.response));
-                }
-            }
-        }
-        request.open("GET", this.UrlMain+url, true);
-        request.send();
-    }
-
-
-    httpPost(url, data, callback)
-    {
-        var request = new XMLHttpRequest();
-        request.onreadystatechange=function()
-        {
-            if (request.readyState==4 && request.status==200)
-            {
-                if(callback != undefined)
-                {
-                    callback(JSON.parse(request.response));
-                }
-            }
-        }
-        request.open("POST", this.UrlMain+url, true);
-        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        request.send( $.param(data) );
-    }
+    
 }
--- client/js/utility.js
+++ client/js/utility.js
@@ -1,4 +1,5 @@
 
+UrlMain = "http://127.0.0.1:8200/"
 
 function loginFast()
 {
@@ -12,8 +13,41 @@ function loginFast()
 }
 
 
+function httpGet(url, callback)
+    {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange=function()
+        {
+            if (request.readyState==4 && request.status==200)
+            {
+                if(callback != undefined)
+                {
+                    callback(JSON.parse(request.response));
+                }
+            }
+        }
+        request.open("GET", this.UrlMain+url, true);
+        request.send();
+    }
 
 
+function httpPost(url, data, callback)
+    {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange=function()
+        {
+            if (request.readyState==4 && request.status==200)
+            {
+                if(callback != undefined)
+                {
+                    callback(JSON.parse(request.response));
+                }
+            }
+        }
+        request.open("POST", this.UrlMain+url, true);
+        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        request.send( $.param(data) );
+    }
 
 /**
  * Aggiungi o sottrai un valore ad una variabile nel css
